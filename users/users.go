c9c0bde216cbf0cfa20452b6c1c2e6216dbbcbe7
takahiroyoshikawa <aihasayahukoma7@gmail.com>
2019-03-02 16:37:44 +0900

    docker-compose version 2 => 3

--- users/users.go
+++ users/users.go
@@ -1,37 +1,41 @@
 package users
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
+	"net/http"
 
-  _ "github.com/go-sql-driver/mysql"
-  "github.com/go-xorm/xorm"
+	"github.com/gin-gonic/gin"
+
+	//mysql driver
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
 )
 
+//Users user info
 type Users struct {
-  ID    int    `xorm:"id"`
-  Name  string `xorm:"name"`
-  Email string `xorm:"email"`
+	ID    int    `xorm:"id"`
+	Name  string `xorm:"name"`
+	Email string `xorm:"email"`
 }
 
+//SignIn user signin function
 func SignIn(c *gin.Context) {
-  //c.JSON(http.StatusOK, gin.H{
-  //  "status": "posted",
-  //  "message": "hello world!!!!!!!!!!",
-  //  "nick": "nicknicknick",
-  //})
-  engine, _ := xorm.NewEngine("mysql", "test:test@tcp(db:3306)/test")
-  user := Users{Name: "yoshikawa", Email: "yoshikawa@mail.com"}
-  _, err := engine.Insert(&user)
-  if err != nil {
-    c.JSON(http.StatusOK, gin.H{
-      "status": "error",
-      "message": err,
-    })
-  } else {
-    c.JSON(http.StatusOK, gin.H{
-      "status": "posted",
-      "message": "success!!!!",
-    })
-  }
+	//c.JSON(http.StatusOK, gin.H{
+	//  "status": "posted",
+	//  "message": "hello world!!!!!!!!!!",
+	//  "nick": "nicknicknick",
+	//})
+	engine, _ := xorm.NewEngine("mysql", "test:test@tcp(db:3306)/test")
+	user := Users{Name: "yoshikawa", Email: "yoshikawa@mail.com"}
+	_, err := engine.Insert(&user)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "error",
+			"message": err,
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "posted",
+			"message": "success!!!!",
+		})
+	}
 }
